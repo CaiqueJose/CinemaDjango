59e07712a9d21c398fda718dd73a732da28ea7d9
CaiqueJose <caiquejosedeoliveira@gmail.com>
2025-12-05 13:07:44 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.views import View
 
-# Create your views here.
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
+class FilmesView(View):
+    def get(self, request, *args, **kwargs):
+        filmes = Filme.objects.all()
+        return render(request, 'filmes.html', {'filmes': filmes})
+# def post(self, request, *args, **kwargs):
+class SeriesView(View):
+    def get(self, request, *args, **kwargs):
+        series = Serie.objects.all()
+        return render(request, 'series.html', {'series': series})
+
+class EpisodiosView(View):
+    def get(self, request, *args, **kwargs):
+        episodios = Episodio.objects.all()
+        return render(request, 'episodios.html', {'episodios': episodios})
+class GenerosView(View):
+    def get(self, request, *args, **kwargs):
+        generos = Genero.objects.all()
+        return render(request, 'generos.html', {'generos': generos})
+class TemporadasView(View):
+    def get(self, request, *args, **kwargs):
+        temporadas = Temporada.objects.all()
+        return render(request, 'temporadas.html', {'temporadas': temporadas})
+class ElencoView(View):
+    def get(self, request, *args, **kwargs):
+        elencos = Elenco.objects.all()
+        return render(request, 'elenco.html', {'elencos': elencos})
+class AtuacaoView(View):
+    def get(self, request, *args, **kwargs):
+        atuacoes = Atuacao.objects.all()
+        return render(request, 'atuacoes.html', {'atuacoes': atuacoes})
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,37 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from app.views import *
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('filmes/', FilmesView.as_view(), name='filmes'),
+path('series/', SeriesView.as_view(),
+name='series'),
+path('episodios/', EpisodiosView.as_view(),
+name='episodios'),
+path('generos/', GenerosView.as_view(), name='generos'),
+path('temporadas/', TemporadasView.as_view(),
+name='temporadas'),
+path('elencos/', ElencoView.as_view(),
+name='elencos'),
+path('atuacoes/', AtuacaoView.as_view(),
+name='atuacoes'),
 ]
